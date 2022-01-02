d8abfa9a6fcf109cd41dc8ca387c6e85075dd735
Demo User <debian@beaglebone-1.ioaire.org>
2022-01-02 16:37:13 -0500

    First commit

---
+++ httpserver.py
@@ -0,0 +1,37 @@
+import subprocess # for piping
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def _writeheaders(self):
+        self.send_response(200) # 200 OK http response
+        self.send_header('Content-type', 'video/mp4')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._writeheaders()
+
+    def do_GET(self):
+        self._writeheaders()
+
+        DataChunkSize = 10000
+
+        command = '(echo "--video boundary--"; raspivid -w 1920 -h 1080 -fps 30 -pf high -n -t 0 -o -;) | gst-launch-1.0 -e -q fdsrc fd=0 ! video/x-h264,width=1920,height=1080,framerate=30/1,stream-format=byte-stream ! h264parse ! mp4mux streamable=true fragment-duration=10 presentation-time=true ! filesink location=/dev/stdout'
+        print("running command: %s" % (command, ))
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1, shell=True)
+
+        print("starting polling loop.")
+        while(p.poll() is None):
+            print "looping... "
+            stdoutdata = p.stdout.read(DataChunkSize)
+            self.wfile.write(stdoutdata)
+
+        print("Done Looping")
+
+        print("dumping last data, if any")
+        stdoutdata = p.stdout.read(DataChunkSize)
+        self.wfile.write(stdoutdata)
+
+if __name__ == '__main__':
+    serveraddr = ('', 8765) # connect to port 8765
+    srvr = HTTPServer(serveraddr, RequestHandler)
+    srvr.serve_forever()

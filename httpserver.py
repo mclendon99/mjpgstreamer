0302666e79155db3a824d0b50d3faf721f8087e2
Demo User <debian@beaglebone-1.ioaire.org>
2022-01-03 17:02:27 -0500

    CLean up server pipe

--- httpserver.py
+++ httpserver.py
@@ -1,9 +1,11 @@
+#! /usr/bin/python3
 import subprocess # for piping
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class RequestHandler(BaseHTTPRequestHandler):
     def _writeheaders(self):
         self.send_response(200) # 200 OK http response
+        self.send_header('Transfer-Encoding', 'chunked')
         self.send_header('Content-type', 'video/mp4')
         self.end_headers()
 
@@ -14,15 +16,14 @@ class RequestHandler(BaseHTTPRequestHandler):
         self._writeheaders()
 
         DataChunkSize = 10000
-
-        command = '(echo "--video boundary--"; ffmpeg -f v4l2 -i /dev/video2 -preset ultrafast -codec copy  \
-                          -f mpegts -segment_format_options movflags=+faststart pipe:1'
+        command = '(echo "--video boundary--"; ffmpeg -f v4l2 -i /dev/video2 -vcodec libx264 -preset ultrafast -tune zerolatency -b:v 900k  -f mpegts -r 30 pipe:1)'
+#        command = '(echo "--video boundary--"; ffmpeg -f v4l2 -fflags +genpts -r 30 -i /dev/video2 -vcodec libx264 -preset veryfast -codec copy  -f segment -segment_format mp4 -segment_time 10 -segment_format_options movflags=+faststart -f mpegts -r 30 pipe:1)'
+#        command = '(echo "--video boundary--"; ffmpeg movie=/dev/video2:f=video4linux -codec copy -f segment -segment_format mp4 -segment_time 10 -segment_format_options movflags=+faststart -f mpegts -r 30 pipe:1)'
         print("running command: %s" % (command, ))
         p = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1, shell=True)
 
         print("starting polling loop.")
         while(p.poll() is None):
-            print("looping... ")
             stdoutdata = p.stdout.read(DataChunkSize)
             self.wfile.write(stdoutdata)
 
@@ -35,4 +36,10 @@ class RequestHandler(BaseHTTPRequestHandler):
 if __name__ == '__main__':
     serveraddr = ('', 8090) # connect to port 8090
     srvr = HTTPServer(serveraddr, RequestHandler)
-    srvr.serve_forever()
+    try:
+       srvr.serve_forever()
+    except KeyboardInterrupt:
+       pass
+
+    srvr.server_close()
+    print("Server stopped")

6d42a4221c3f3cef83e1074ecd93f7e4a1ba1fb3
Demo User <debian@beaglebone-1.ioaire.org>
2022-01-03 13:39:38 -0500

    Use ffmpeg

--- httpserver.py
+++ httpserver.py
@@ -15,7 +15,8 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         DataChunkSize = 10000
 
-        command = '(echo "--video boundary--"; raspivid -w 1920 -h 1080 -fps 30 -pf high -n -t 0 -o -;) | gst-launch-1.0 -e -q fdsrc fd=0 ! video/x-h264,width=1920,height=1080,framerate=30/1,stream-format=byte-stream ! h264parse ! mp4mux streamable=true fragment-duration=10 presentation-time=true ! filesink location=/dev/stdout'
+        command = '(echo "--video boundary--"; ffmpeg -f v4l2 -i /dev/video2 -preset ultrafast -codec copy  \
+                          -f mpegts -segment_format_options movflags=+faststart pipe:1'
         print("running command: %s" % (command, ))
         p = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1, shell=True)
 
@@ -32,6 +33,6 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(stdoutdata)
 
 if __name__ == '__main__':
-    serveraddr = ('', 8765) # connect to port 8765
+    serveraddr = ('', 8090) # connect to port 8090
     srvr = HTTPServer(serveraddr, RequestHandler)
     srvr.serve_forever()

ecec306b92203f6ecbfb9772f6d92f285fe4f613
mclendon99 <postmasters@bellsouth.net>
2022-01-11 15:40:55 -0500

    Fixed lot's of little bugs Cleaned up config handling Fixed up logging

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 
 ### MJPGStreamer
 
-MJPGStreamer is a streamer that can stream MJPG (obviously) from a USB or standard camera. It is based loosely on soyersoyers [FMP4Streamer][1] 'cause he's a decent coder and I could borrow lot's of the infrastructure.  Most of the functional insides have been rewritten.
+MJPGStreamer is a streamer that can stream MJPG (obviously) from a USB or standard camera. It is based loosely on soyersoyers [FMP4Streamer][1] 'cause he's a decent coder and I could borrow lot's of his infrastructure/design.  Most of the functional insides have been rewritten.
 
-The v4l2py library was also used. Be sure to patch the v4l2 library as indicated here [V4L2 patch][3]
+The [v4l2py][2] library was also used. Be sure to patch the v4l2 library as indicated here [V4L2 patch][3]
 
 #### Features
-- Lightweight - around 10% CPU on a single core 500 MHz ARMv7
-- Built on Python3.7 on a Beaglebone Black 5.10.87-bone59
-- Based on v4l2py 0.6.0
+- Lightweight - around 12% CPU on a single core 500 MHz ARMv7 capturing 640x480 30fps
+  - UVC driver adds another 5%
+- Built using Python3.7 on a Beaglebone Black 5.10.87-bone59
+- Based on v4l2py 0.6.0 [2]
 - Supports multiple browser connections
 - Supports USB uv4l cameras
  - Be sure to apply the patch [V4L2 patch][3]
@@ -22,9 +23,8 @@ The v4l2py library was also used. Be sure to patch the v4l2 library as indicated
 
 #### Todo
 - Support configuration of multiple servers on different ports, e.g. HTTP and HTTPS
-- Support multiple cameras of course
+- Support multiple cameras (of course)
 - Test on the RPI with the MIPI camera
-
 
 [1]https://github.com/soyersoyer/fmp4streamer
 [2]https://pypi.org/project/v4l2py/
--- mjpgstreamer.py
+++ mjpgstreamer.py
@@ -43,10 +43,10 @@ class CameraThread(threading.Thread):
         super(CameraThread, self).__init__()
         self.camera = camera
         self.consumers = []
-        logging.debug(f'Camera initialized')
+        logger.debug(f'Camera initialized')
 
     def run(self):
-        logging.debug(f'Camera running')
+        logger.debug(f'Camera running')
         frame_budget = 1000 / fps
         for frame in self.camera:
            start = timeit.default_timer()
@@ -58,17 +58,22 @@ class CameraThread(threading.Thread):
               time.sleep(diff/1000) # Wait until next frame ready
 
     def stop(self):
-        logging.debug(f'Camera stopped')
+        logger.debug(f'Camera stopped')
         self.frame = None
         self.consumers.clear()
 
     def register_queue(self, q):
         self.consumers.append(q)
-        logging.debug(f'Register consumer')
+        logger.debug(f'Register consumer')
 
     def unregister_queue(self, q):
         self.consumers.remove(q)
-        logging.debug(f'Deregister consumer')
+        logger.debug(f'Deregister consumer')
+
+    def shutdown(self):
+        self.stop()
+        camera.close()
+        sys.exit(0)
 
 class HTTPServer(socketserver.ThreadingMixIn, HTTPServer):
     allow_reuse_address = True
@@ -78,9 +83,10 @@ class HTTPServer(socketserver.ThreadingMixIn, HTTPServer):
       try:
           self.serve_forever()
       except KeyboardInterrupt:
-          pass
-      finally:
-          self.server_close()
+          return
+
+    def shutdown(self):
+      self.server_close()
 
 class RequestHandler(BaseHTTPRequestHandler):
     def process_camera_frames(self):
@@ -94,29 +100,34 @@ class RequestHandler(BaseHTTPRequestHandler):
               try:
                 self.wfile.write(b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
               except Exception as e:
-                logging.warning(f'Removing streaming client {self.client_address} {str(e)}')
+                logger.warning(f'Removing streaming client {self.client_address} {str(e)}')
                 try:
                   self.send_error(404)
                   self.end_headers()
                   return 0
                 except Exception as e:
-                  logging.info(f'Send 404 failed {self.client_address} {str(e)}')
+                  logger.info(f'Send 404 failed {self.client_address} {str(e)}')
                   return 0 # Don't kill everything. Go back and wait for new connection
             else:
               try: 
-                logging.info(f'Camera shutdown detected {self.client_address} {str(e)}')
+                logger.info(f'Camera shutdown detected {self.client_address} {str(e)}')
                 self.send_response(404)
                 self.end_headers()
                 return -2
               except Exception as e:
-                logging.warning(f'Send 404 failed {self.client_address} {str(e)}')
+                logger.warning(f'Send 404 failed {self.client_address} {str(e)}')
+                return -2
+          except KeyboardInterrupt:
+                self.send_response(404)
+                self.end_headers()
+                self.shutdown()
                 return -2
           except Exception as e:
-              logging.info(f'Empty queue timeout {str(e)}')
+              logger.info(f'Empty queue timeout {str(e)}')
               time.sleep(frame_budget/1000) # Wait until next frame ready
                   
     def do_GET(self):
-        logging.debug(f'do_GET from {self.client_address}')
+        logger.debug(f'do_GET from {self.client_address}')
         self.send_response(200)
         self.send_header('Age','0')
         self.send_header('Expires','0')
@@ -127,12 +138,14 @@ class RequestHandler(BaseHTTPRequestHandler):
         ## Register our queue and start sending frames to the client
         self.q = Queue()
         camera_thread.register_queue(self.q)
-        logging.debug(f'do_GET Register client {self.client_address}')
+        logger.debug(f'do_GET Register client {self.client_address}')
         rc = self.process_camera_frames()
-        logging.debug(f'do_GET Unregister client {self.client_address}')
+        logger.debug(f'do_GET Unregister client {self.client_address}')
         camera_thread.unregister_queue(self.q)
-        logging.debug(f'do_GET - exit thread {self.client_address}:{rc}')
+        logger.debug(f'do_GET - exit thread {self.client_address}:{rc}')
         sys.exit(rc)
+
+
 
 class Config(configparser.ConfigParser):
     def __init__(self, configfile):
@@ -148,7 +161,7 @@ class Config(configparser.ConfigParser):
                        'certfile':''}})
 
         if len(self.read(configfile)) == 0:
-            logging.warning(f'Couldn\'t read {configfile}, using default config')
+            logger.warning(f'Couldn\'t read {configfile}, using default config')
 
         if len(self.sections()) == 2:
             self.add_section('/dev/video0')
@@ -180,42 +193,55 @@ class Config(configparser.ConfigParser):
         return config['default']['logfile']
 
     def loglevel(self):
-        return config['default']['logfile']
+        return config['default']['loglevel']
+
 
 def usage():
-    print(f'usage: python3 {sys.argv[0]} [--help] [--list-controls] [--config CONFIG]\n')
-    print(f'optional arguments:')
-    print(f'  -h, --help                  show this help message and exit')
-    print(f'  -l, --list-controls         list the v4l2 controls and values for the camera')
-    print(f'  -c CONFIG, --config CONFIG  use CONFIG as a config file, default: mjpgstreamer.conf')
-    print(f'  -d {DEBUG|INFO|WARNING|ERROR|CRITICAL}   default: WARNING')
+    print(f'Usage: python3 {sys.argv[0]} [--help] [--config CONFIG]\n')
+    print(f'Optional arguments:')
+    print(f'  -h, --help                  Show this help message and exit')
+    print(f'  -i, --info                  Show video information and exit')
+    print(f'  -c CONFIG, --config CONFIG  Use CONFIG as a config file, default: mjpgstreamer.conf')
+    print(f'  -d D(EBUG)|I(NFO)|W(ARNING)|E(RROR)|C(RITICAL)   default: WARNING - Only D,I,W options implemented')
 
 
 if __name__ == '__main__':
  
+    # Command line handling
     try:
-        arguments, values = getopt.getopt(sys.argv[1:], "hlcd:", ["help", "list-controls","config=","debug="])
+        arguments, values = getopt.getopt(sys.argv[1:], "hicd:", ["help", "info", "config=","debug="])
     except getopt.error as err:
         print(err)
         usage()
         sys.exit(-1)
     
     configfile = "mjpgstreamer.conf"
-    list_controls = False
+    list_info = False
+    log_dict = {'D':'DEBUG',
+                'I':'INFO',
+                'W':'WARNING',
+                'E':'ERROR',
+                'C':'CRITICAL'}
     for current_argument, current_value in arguments:
         if current_argument in ('-h', '--help'):
             usage()
             sys.exit(0)
-        elif opt in( '-d', '--debug'):
-            loglevel = arg
-            print("Using log level:"+loglevel)
-        elif current_argument in ("-l", "--list-controls"):
-            list_controls = True
         elif current_argument in ("-c", "--config"):
             configfile = current_value
+        elif current_argument in ("-i", "--info"):
+            list_info = True
+        elif current_argument in( '-l', '--log'):
+            loglevel = upper(arg)
+            if loglevel not in log_dict:
+               print(f'Invalid log argument {loglevel}')
+               sys.exit(-1)
+            loglevel = log_dict(loglevel)
+            print(f'Selected log level:{loglevel}')
     
-    # Some defaults
+    # Config file handling
+    # Set defaults as per config file, if any
     config = Config(configfile)
+
     device = config.get_device()
 
     logfile = config.logfile()
@@ -228,42 +254,50 @@ if __name__ == '__main__':
     width = config.width()
     fps =  config.fps()
 
-    # Process debug level arg
+    # Set up logging using "logger"
+    # Process log level arg
     log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
 
     my_handler = RotatingFileHandler(logfile, mode='a', maxBytes=5*1024*1024,
                                    backupCount=2, encoding=None, delay=0)
     my_handler.setFormatter(log_formatter)
-    numericLevel = getattr(logging, loglevel.upper(),"WARNING")
-    print("Using log level:" + str(numericLevel))
+    numericLevel = getattr(logging, loglevel,"WARNING")
+    print(f'Logging:{loglevel} -> {logfile}')
     my_handler.setLevel(numericLevel)
 
-    app_log = logging.getLogger('root')
-    app_log.setLevel(numericLevel)
-    app_log.addHandler(my_handler)
-
-    logging.info(f'Using device {device}')
-    print(f'Using device {device}')
+    logger = logging.getLogger('root')
+    logger.setLevel(numericLevel)
+    logger.addHandler(my_handler)
 
     camera = Device(device)
-    if list_controls:
-        print(camera.video_capture.get_format())
-        print(camera.video_capture.get_fps(),' FPS')
+    # Use settings from the config file
+    camera.video_capture.set_format(width, height) 
+    camera.video_capture.set_fps(fps) 
+    # Copy what the camera thinks
+    width,height,fmt = camera.video_capture.get_format()
+
+    logger.info(f'Using device {device} {width}x{height}@{camera.video_capture.get_fps()} FPS')
+    print(f'Using device {device} {width}x{height}@{camera.video_capture.get_fps()} FPS')
+
+    if list_info:
+        print(f'{width}x{height}@{camera.video_capture.get_fps()} FPS')
         info = str(camera.info)
         print(info.replace(',','\n'))
         sys.exit(0)
 
     camera_thread = CameraThread(camera)
-    logging.debug(f'Camera starts')
+    logger.debug(f'Camera starts')
     camera_thread.start()
 
     srvr = HTTPServer((hostname,hostport), RequestHandler)
-    logging.debug(f'Server Starts - {hostname}:{hostport}')
+    logger.debug(f'Server Starts - {hostname}:{hostport}')
     if len(config.keyfile()) != 0 and len(config.certfile()) != 0 :
         srvr.socket = ssl.wrap_socket(srvr.socket, \
                config.keyfile(), config.certfile(), server_side=True)
 
     srvr.start()
+    srvr.shutdown()
+    camera_thread.shutdown()
     camera_thread.join()
    
-    logging.debug(f'Server Stops - {hostname}:{hostport}')
+    logger.debug(f'Server Stops - {hostname}:{hostport}')
